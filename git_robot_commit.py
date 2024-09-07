fde636dac1ef731e81c981a9c0f88443f53ace26
Andy Holaday <smartin108@gmail.com>
2024-09-07 15:24:24 -0400

    automatic commit

--- git_robot_commit.py
+++ git_robot_commit.py
@@ -8,7 +8,8 @@ import subprocess
 def get_list_of_root_folders():
     """ return catalog of enumerated git repo roots
     """
-    return [r'C:\Users\Z40\Documents\git', r'C:\Users\Z40\Documents\batch',r"C:\Users\Z40\AppData\Roaming\REAPER\Effects\smartin"]
+    # return [r'C:\Users\Z40\Documents\git', r'C:\Users\Z40\Documents\batch',r"C:\Users\Z40\AppData\Roaming\REAPER\Effects\smartin"]
+    return[r"C:\Users\Z40\Documents\git\AudioAnalysis"]
 
 
 def filter_repo_subpaths():
@@ -21,13 +22,17 @@ def get_list_of_repos(root_folder:str):
     """
     # return [r'C:\Users\Z40\Documents\git', r'C:\Users\Z40\Documents\batch',r"C:\Users\Z40\AppData\Roaming\REAPER\Effects\smartin"]
     result = set()
-    for p, d, _ in os.walk(root_folder):
+    for p, d, f in os.walk(root_folder):
         if '.git' in p:
             test = False
-            # print(f'rejected: {(p,d)}')
-        else:
+            # print(f'rejected: {(p,d,f)}')
+        elif '.git' in d:
             test = True
             result.add(p)
+            # print(f'accepted: {(p,d,f)}')
+        else:
+            test = False
+            # print(f'rejected: {(p,d,f)}')
     # print(f'\n{result}')
     return result
 
@@ -47,7 +52,7 @@ def commit_repo(path_to_git_project:str, commit_message:str='"automatic commit"'
     result = [shell_command(cd_command)]
     result.append(shell_command(git_add_command))
     result.append(shell_command(git_commit_command))
-    print(result)
+    # print(result)
 
     return result
 
