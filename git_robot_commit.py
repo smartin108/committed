e614ded8e4baadecda8611701e478807dac2996e
Andy Holaday <smartin108@gmail.com>
2024-09-07 22:22:21 -0400

    automatic commit

--- git_robot_commit.py
+++ git_robot_commit.py
@@ -8,8 +8,8 @@ import subprocess
 def get_list_of_root_folders():
     """ return catalog of enumerated git repo roots
     """
-    # return [r'C:\Users\Z40\Documents\git', r'C:\Users\Z40\Documents\batch',r"C:\Users\Z40\AppData\Roaming\REAPER\Effects\smartin"]
-    return[r"C:\Users\Z40\Documents\git\audiodabble"]
+    return [r'C:\Users\Z40\Documents\git', r'C:\Users\Z40\Documents\batch',r"C:\Users\Z40\AppData\Roaming\REAPER\Effects\smartin"]
+    # return[r"C:\Users\Z40\Documents\git\audiodabble"]
 
 
 def filter_repo_subpaths():
@@ -41,18 +41,19 @@ def commit_repo(path_to_git_project:str, commit_message:str='"automatic commit"'
     """
     """
 
-    def shell_command(command_to_execute:str):
+    def shell_command(command_to_execute:str, cwd:str=path_to_git_project):
         full_shell_command = f'cmd.exe /c {command_to_execute}'
-        return subprocess.run(full_shell_command, capture_output=True, text=True, shell=True)
+        return subprocess.run(full_shell_command, capture_output=True, text=True, shell=True, cwd=cwd)
 
-    cd_command = f'cd {path_to_git_project}\\'
+    # cd_command = f'cd {path_to_git_project}\\'
     git_status_command = 'git status'
     git_add_command = 'git add .'
     git_commit_command = f'git commit -m {commit_message}'
     git_log_command = 'git log'
 
-    result = [shell_command(cd_command)]
-    result.append(shell_command(git_status_command))
+    # result = [shell_command(cd_command)]
+    # result.append(shell_command(git_status_command))
+    result =  [shell_command(git_status_command)]
     result.append(shell_command(git_add_command))
     result.append(shell_command(git_commit_command))
     result.append(shell_command(git_status_command))
