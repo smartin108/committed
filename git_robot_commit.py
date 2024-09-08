4626279c5e50f8e4def2acdc9703f1968e25db57
Andy Holaday <smartin108@gmail.com>
2024-09-07 22:35:42 -0400

    automatic commit

--- git_robot_commit.py
+++ git_robot_commit.py
@@ -9,31 +9,20 @@ def get_list_of_root_folders():
     """ return catalog of enumerated git repo roots
     """
     return [r'C:\Users\Z40\Documents\git', r'C:\Users\Z40\Documents\batch',r"C:\Users\Z40\AppData\Roaming\REAPER\Effects\smartin"]
-    # return[r"C:\Users\Z40\Documents\git\audiodabble"]
-
-
-def filter_repo_subpaths():
-    """ return True or False: folder is (True) considered a legitimate candidate as a repo top-level folder, or (False) not so
-    """
 
 
 def get_list_of_repos(root_folder:str):
     """ return a list of paths at and below <root_folder> that contain a git repo
     """
-    # return [r'C:\Users\Z40\Documents\git', r'C:\Users\Z40\Documents\batch',r"C:\Users\Z40\AppData\Roaming\REAPER\Effects\smartin"]
     result = set()
     for p, d, f in os.walk(root_folder):
         if '.git' in p:
             test = False
-            # print(f'rejected: {(p,d,f)}')
         elif '.git' in d:
             test = True
             result.add(p)
-            # print(f'accepted: {(p,d,f)}')
         else:
             test = False
-            # print(f'rejected: {(p,d,f)}')
-    # print(f'\n{result}')
     return result
 
 
@@ -45,20 +34,13 @@ def commit_repo(path_to_git_project:str, commit_message:str='"automatic commit"'
         full_shell_command = f'cmd.exe /c {command_to_execute}'
         return subprocess.run(full_shell_command, capture_output=True, text=True, shell=True, cwd=cwd)
 
-    # cd_command = f'cd {path_to_git_project}\\'
     git_status_command = 'git status'
     git_add_command = 'git add .'
     git_commit_command = f'git commit -m {commit_message}'
     git_log_command = 'git log'
 
-    # result = [shell_command(cd_command)]
-    # result.append(shell_command(git_status_command))
-    # result =  [shell_command(git_status_command)]
-    # result.append(shell_command(git_add_command))
     result = [shell_command(git_add_command)]
     result.append(shell_command(git_commit_command))
-    # result.append(shell_command(git_status_command))
-    # result.append(shell_command(git_log_command))
     for i in result:
         print(i)
 
